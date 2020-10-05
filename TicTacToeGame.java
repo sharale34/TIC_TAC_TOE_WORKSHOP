fa53446a02849108c7535809db1fdbee2413c149
sharale34 <sourabhharale8000@gmail.com>
2020-10-05 08:23:06 +0530

    Added: Ability To Allow Player To Choose Letter

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,13 +1,25 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	// Variables
+	private static char computer;
 	private static char[] board;
+	private static char userLetter;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To Tic-Tac-Toe Game");
 		board = createBoard();
+		userLetter = chooseUserLetter();
+
+		// Assigning Letter to Computer
+		if (userLetter == 'X')
+			computer = 'O';
+		else
+			computer = 'X';
+		System.out.println("The Computer Letter is :"+computer);
 	}
 
 	// Create Board and Assign Empty Spaces
@@ -16,5 +28,13 @@ public class TicTacToeGame {
 		for (int index = 1; index < board.length; index++)
 			board[index] = ' ';
 		return board;
+	}
+
+	// Selection of Letter by User
+	private static char chooseUserLetter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose Your User Letter X or O");
+		char userLetter = sc.next().toUpperCase().charAt(0);
+		return userLetter;
 	}
 }
