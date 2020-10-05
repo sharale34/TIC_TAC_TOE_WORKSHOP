65f53e6a70f9a7c7ac834e5e1e8106ea28e50070
sharale34 <71629896+sharale34@users.noreply.github.com>
2020-10-05 17:49:52 +0530

    Added: Whether The Player Wants To Play Another Game

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -238,7 +238,7 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe Game");
 		boolean restart = askIfPlayerWantsToRestart();
-		while (restart!= false) {
+		while (restart) {
 			board = createBoard();
 			userLetter = chooseUserLetter();
 
